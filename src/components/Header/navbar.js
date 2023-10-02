192d7ff78e3e68f9854e08693716f2ef0a3bf7c3
CAMARA <88945535+camara92@users.noreply.github.com>
2023-10-02 19:59:04 +0200

    commpoents

---
+++ src/components/Header/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='banner-container'>
+        <h1>Banner </h1>
+      </div>
+      <h3>Header </h3>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/Landing/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <main className='welcomePage'>
+        <h2>Daouda Salut ! </h2>
+
+    </main>
+  )
+}
+
+export default Landing;
