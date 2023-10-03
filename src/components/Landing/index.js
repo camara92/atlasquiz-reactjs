4814de583b5b790a74deeb711f5932e7f159f3a5
CAMARA <88945535+camara92@users.noreply.github.com>
2023-10-03 02:28:28 +0200

    landing page useeffect, usestate and react fragment

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,28 +1,38 @@
-import React, {useRef, useEffect, useState} from 'react'
+import React, { useRef, useEffect, useState, Fragment } from 'react'
 
 const Landing = () => {
- const [btn, setBtn] = useState(false); 
-  const refWolverine = useRef(null); 
-
-  useEffect(()=>{
-    // elle execute au moment du montage : 
-    // console.log("Je suis dans le useEffect ! ")
-    refWolverine.current.classList.add("startingImg");   
+  const [btn, setBtn] = useState(false);
+  const refWolverine = useRef(null);
+  console.log(btn);
+  useEffect(() => {
+    refWolverine.current.classList.add("startingImg");
+    // refWolverine.current.textContent= "Daouda"; 
     setTimeout(() => {
-      // enlever au bout de 4s 
-      refWolverine.current.classList.remove("startingImg");   
-    }, 4000);
+      
+      refWolverine.current.classList.remove("startingImg");
+      // refWolverine.current.textContent= ""; 
+      setBtn(true);
+    }, 1000);
   }, [])
-   console.log(refWolverine );
-  return (
-    <main ref= {refWolverine}  className='welcomePage'>
-        <div className='leftBox'>
-          <button className='btn-welcome'>Inscription</button>
-        </div>
-        <div className='rightBox'>
-          <button className='btn-welcome'>Connexion </button>
-        </div>
 
+
+
+  const displayBtn = btn && (
+
+    <Fragment>
+
+      <div className='leftBox'>
+        <button className='btn-welcome'>Inscription</button>
+      </div>
+      <div className='rightBox'>
+        <button className='btn-welcome'>Connexion </button>
+      </div>
+    </Fragment>
+  )
+  //  console.log(refWolverine );
+  return (
+    <main ref={refWolverine} className='welcomePage'>
+      {displayBtn }
     </main>
   )
 }
