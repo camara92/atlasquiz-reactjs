cfd971aa27c7bc0e6735726526427e66e5c9676b
CAMARA <88945535+camara92@users.noreply.github.com>
2023-10-04 16:08:03 +0200

    npm i --save firebase

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import errorPageImlage from '../images/errorPageImg.jpg'
+
+const centerH2 = {
+  textAlign: "center", 
+  marginTop: "50px"
+}
+const centerImg = {
+  display: "block", 
+  marginTop: "40px auto", 
+  borderRadius : "50% 50%",  
+  height: 800
+}
 
 const ErrorPage = () => {
   return (
     <div className='quiz-bg'>
       <div className='container'> 
-      ErrorPage 
+      <h2 style={centerH2}>Oups, cette page n'existe pas ! </h2>
+       <img style={centerImg } src={errorPageImlage} alt='pageError' />
       </div>
     </div>
   )
